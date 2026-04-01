de8424b80340add59f6c5d0a0292bfd149aec354
Holger Seelig <holger.seelig@gmail.com>
2026-04-01 17:24:50 +0200

    Added unit categories.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
+# 1.0.2
+
+- Better UNIT support
+
 ## 1.0.0
+
 - Initial release
--- build/snippets.js
+++ build/snippets.js
@@ -37,7 +37,7 @@ async function main ()
          prefix: typeName,
          description: "",
          body: [
-            typeName + " { }",
+            typeName + " { ${1:}}",
          ],
       };
    }
